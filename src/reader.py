88e83dd43b81ab9311064c2de3e1cc4f1876badb
Ciro Callazans <74212395+ccallazans@users.noreply.github.com>
2024-09-03 21:31:42 -0300

    ajuste reader.py

--- src/reader.py
+++ src/reader.py
@@ -4,9 +4,9 @@ def read_csv(file_path):
     tasks = []
     with open(file_path, mode='r') as file:
         csv_reader = csv.DictReader(file)
-        for row in csv_reader:
+        for index, row in enumerate(csv_reader, start=1):
             task = {
-                'Task': int(row['Task']),
+                'Task': f'T{index}',
                 'Custo': int(row['Custo']),
                 'Periodo': int(row['Periodo'])
             }
